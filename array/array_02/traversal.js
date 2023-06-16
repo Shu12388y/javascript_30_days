b8a6d0eaed72e8a0a798074faeb4435e9f31f8c9
Shu12388y <spl.sp999@gmail.com>
2023-06-16 17:32:48 +0530

    new file added

--- array/array_02/traversal.js
+++ array/array_02/traversal.js
@@ -108,3 +108,114 @@ names.forEach(function(element,index,array){
 // let us see the indexof method
 
 
+
+// =======================================================================================================================================
+
+// let us see the push() method
+
+names.push("deepak")
+console.log(names)
+
+
+// push() method  adds one or more elements to the end of an array and returns the new length of the array
+
+// =======================================================================================================================
+
+
+ //let us see unshift() method
+
+//unshift method adds one or more elements to the begining of an array and return the new length of the array
+
+
+ names.unshift("neoman")
+ console.log(names)
+
+
+// =======================================================================================================================
+
+  //pop() method
+
+  //it remove the last element of the array and return that element and this method change the lenght of the array
+
+  names.pop()
+
+// =======================================================================================================================
+
+
+
+  // shift() method .
+  //It remove the element from the front of the array
+
+
+  names.shift()
+
+
+// =======================================================================================================================
+
+
+
+// Challenge 
+
+
+
+// Given array
+//1. Add Dec at the end of array
+//2. Update march to March
+//3. delete June from the array
+
+const months=["jan","Feb","march","April","June","July"]
+
+
+
+// months.push("Dec")
+months[2]="March"
+months[4]="Junly"
+months[5]="Dec"
+
+
+const newer=months
+console.log(newer)
+
+
+//output:  [ 'jan', 'Feb', 'March', 'April', 'Junly', 'Dec' ]
+
+
+
+// =======================================================================================================================
+
+
+// let us see the slice() method
+//slice method used to delete the elements from an array and it return an array of deleted elements
+
+// slice(index_postion_of_starting,number_of_element_to_delete,"string to add")
+
+
+//let solve the above problem using slice method
+
+// let us add the "Dec at end"
+
+months.slice(months.length,0,"Dec")
+
+
+//Now update the "march"
+
+const findIndex=months.indexOf("march")
+months.slice(findIndex,1,"March")
+
+
+// Now delete "June"
+
+const findsIndex=months.indexOf("June")
+months.slice(findsIndex,1)
+
+
+
+console.log(months)
+
+
+// output: [ 'jan', 'Feb', 'March', 'April', 'Junly', 'Dec' ]
+
+// =======================================================================================================================
+
+
+
