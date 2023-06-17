5113571a2e6bbffca1b33b18b51fe222ab948b7f
Shu12388y <spl.sp999@gmail.com>
2023-06-17 14:23:26 +0530

    string folder added

---
+++ string/string.js
@@ -0,0 +1,23 @@
+//====================================== String in javascript  ==================================================================================
+
+
+
+
+const names="shubham paul"   //String is a primitive data type
+const degree="B.Tech"
+
+console.log(names)
+console.log(degree)
+
+
+//let us create a array
+const bio_data=["Hello ","my","name","is","shubham"]
+
+bio_data.push(`is a ${degree} degree student`)
+console.log(bio_data)
+
+//let us find the length of the string
+
+//For that we use .length propert
+
+console.log(names.length)
